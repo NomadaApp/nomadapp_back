2da172ff3895bda2c4ea33c5eb117f7605f754a0
Manuel Castillo <manucalop@gmail.com>
2022-03-01 15:21:51 +0100

    Added test to deployed gcp backend

---
+++ tests/test_gcp_request.py
@@ -0,0 +1,14 @@
+import unittest
+import requests
+
+
+class TestGCPRequest(unittest.TestCase):
+    def test_hello(self):
+        url = "https://nomadapp-back-akukb5qdcq-ew.a.run.app/json-request"
+        response = requests.get(url, json=None)
+        print("JSON content:", response.json())
+        self.assertEqual({"test": 1}, response.json(), "I leave Python!")
+
+
+if __name__ == "__main__":
+    unittest.main()
