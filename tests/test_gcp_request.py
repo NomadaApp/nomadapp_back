9c06c6fe95ac39d12dfb4b33663edc52634b97f3
Manuel Castillo <manucalop@gmail.com>
2022-03-03 10:11:01 +0100

    Fixed unit test

--- tests/test_gcp_request.py
+++ tests/test_gcp_request.py
@@ -1,10 +1,21 @@
 import unittest
 import requests
+from nomadapp_back import flask_api
 
 
-class TestGCPRequest(unittest.TestCase):
+class TestApiRequest(unittest.TestCase):
     def test_hello(self):
         url = "https://nomadapp-back-akukb5qdcq-ew.a.run.app/json-request"
+        query = {
+            "leisure": False,
+            "restaurants": True,
+            "education": True,
+            "coworking": False,
+            "radius": 5,
+            "location": "Puerta del Sol, 1",
+        }
+        # response = requests.get(url, params=query)
+        # url = "https://nomadapp-back-akukb5qdcq-ew.a.run.app/json-request"
         # response = requests.get(url, json=None)
         # self.assertEqual({"test": 1}, response.json(), "I leave Python!")
 
